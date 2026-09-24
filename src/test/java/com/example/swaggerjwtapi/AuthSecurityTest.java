0d9bded0fd452d12588f1e3f8776abc4c1c243c9
miguel alcantara <miguel@mike.local>
2026-09-23 21:56:50 -0500

    some weird changes i dont remember

--- src/test/java/com/example/swaggerjwtapi/AuthSecurityTest.java
+++ src/test/java/com/example/swaggerjwtapi/AuthSecurityTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AuthSecurityTest {
 
     @Autowired
@@ -38,8 +40,6 @@ public class AuthSecurityTest {
                         .andExpect(jsonPath("$.accessToken").exists())
                         .andExpect(jsonPath("$.refreshToken").exists())
                         .andReturn();
-
-        System.out.println("login OK");
     }
 
     @Test
